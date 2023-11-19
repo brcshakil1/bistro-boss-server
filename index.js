81591b453ee091a68a20e967a87520a905ba2a5e
brcshakil1 <brcshakil72@gmail.com>
2023-11-19 23:46:18 +0600

    adding stripe payment method

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const app = express();
 const port = process.evn || 5000;
 
@@ -224,6 +226,20 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = cartsCollection.deleteOne(query);
       res.send(result);
+    });
+
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     });
 
     // Send a ping to confirm a successful connection
